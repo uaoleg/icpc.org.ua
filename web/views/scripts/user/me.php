64a659c5aade358a5d8689240eef4b5dfcf48673
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-10-10 18:50:30 +0300

    #40 Showed Coordinator type on profile page

--- web/views/scripts/user/me.php
+++ web/views/scripts/user/me.php
@@ -1,17 +1,5 @@
 <div class="col-lg-offset-4 col-lg-5">
 
-    <?php if (!empty($user->coordinator)): ?>
-        <?php if (\yii::app()->user->checkAccess(\common\models\User::ROLE_COORDINATOR)): ?>
-            <div class="alert alert-success">
-                <?=\yii::t('app', 'You are approved coordinator.')?>
-            </div>
-        <?php else: ?>
-            <div class="alert alert-warning">
-                <?=\yii::t('app', 'Waiting approval of coordinator request.')?>
-            </div>
-        <?php endif; ?>
-    <?php endif; ?>
-
     <table class="table table-bordered">
         <tr>
             <td>
@@ -34,7 +22,23 @@
                 <?=\yii::t('app', 'User status')?>
             </td>
             <td>
-                <?=$user->type?>
+                <?php if (!empty($user->type)): ?>
+                    <?=$user->type?>
+                    <br />
+                <?php endif; ?>
+                <?php if (!empty($user->coordinator)): ?>
+                    <?=\yii::t('app', 'Coordination')?>: <?=$user->getAttributeLabel($user->coordinator, 'coord')?>
+                    <?php if (\yii::app()->user->checkAccess(\common\models\User::ROLE_COORDINATOR)): ?>
+                        <span class="label label-success">
+                            <?=\yii::t('app', 'Approved')?>
+                        </span>
+                    <?php else: ?>
+                        <span class="label label-warning">
+                            <?=\yii::t('app', 'Waiting approval')?>
+                        </span>
+                    <?php endif; ?>
+                    <br />
+                <?php endif; ?>
             </td>
         </tr>
     </table>
