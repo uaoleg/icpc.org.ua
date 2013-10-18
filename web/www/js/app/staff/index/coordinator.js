6ac3536f18a93953ed1924d8a8d4c1b93a243306
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-10-18 11:06:03 +0300

    #48 Fixed Suspend/Reactivate buttons behavior

--- web/www/js/app/staff/index/coordinator.js
+++ web/www/js/app/staff/index/coordinator.js
@@ -2,8 +2,9 @@ function appStaffIndexCoordinator() {
 
     // Set coordinator state
     $('.btn.coordinator-state').on('click', function() {
-        var $this = $(this);
-        $('.btn.coordinator-state').removeClass('hide');
+        var $this = $(this),
+            $td = $this.closest('td');
+        $('.btn.coordinator-state', $td).removeClass('hide');
         $this.addClass('hide');
         $.ajax({
             url: app.baseUrl + '/staff/index/coordinatorSetState',
