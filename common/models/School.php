87940bf5da5b6ea379a8631883293e66036734a6
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-10-25 17:50:52 +0300

    #71 Code style improvements

--- common/models/School.php
+++ common/models/School.php
@@ -2,26 +2,29 @@
 
 namespace common\models;
 
-class School extends \common\ext\MongoDb\Document {
-
+/**
+ * School (university, academy, institute)
+ */
+class School extends \common\ext\MongoDb\Document
+{
 
     /**
      * Full university name in ukranian
      * @var string
      */
-    public $fullNameUa;
-
-    /**
-     * Short university name in ukrainian
-     * @var string
-     */
-    public $shortNameUa;
+    public $fullNameUk;
 
     /**
      * Full university name in english
      * @var string
      */
     public $fullNameEn;
+
+    /**
+     * Short university name in ukrainian
+     * @var string
+     */
+    public $shortNameUk;
 
     /**
      * Short university name in english
@@ -42,16 +45,6 @@ class School extends \common\ext\MongoDb\Document {
     public $region;
 
     /**
-     * This returns the name of the collection for this class
-     *
-     * @return string
-     */
-    public function getCollectionName()
-    {
-        return 'school';
-    }
-
-    /**
      * Returns the attribute labels.
      *
      * Note, in order to inherit labels defined in the parent class, a child class needs to
@@ -62,12 +55,12 @@ class School extends \common\ext\MongoDb\Document {
     public function attributeLabels()
     {
         return array_merge(parent::attributeLabels(), array(
-            'fullNameUa' => \yii::t('app', 'Full university name in ukrainian'),
-            'shortNameUa' => \yii::t('app', 'Short university name in ukrainian'),
-            'fullNameEn' => \yii::t('app', 'Full university name in english'),
-            'shortNameEn' => \yii::t('app', 'Short university name in english'),
-            'state' => \yii::t('app', 'State'),
-            'region' => \yii::t('app', 'Region'),
+            'fullNameUk'    => \yii::t('app', 'Full university name in ukrainian'),
+            'fullNameEn'    => \yii::t('app', 'Full university name in english'),
+            'shortNameUk'   => \yii::t('app', 'Short university name in ukrainian'),
+            'shortNameEn'   => \yii::t('app', 'Short university name in english'),
+            'state'         => \yii::t('app', 'State'),
+            'region'        => \yii::t('app', 'Region'),
 
         ));
     }
@@ -80,9 +73,73 @@ class School extends \common\ext\MongoDb\Document {
     public function rules()
     {
         return array_merge(parent::rules(), array(
-            array('fullNameUa, state, region', 'required'),
-            array('fullNameUa', 'unique')
+            array('fullNameUk, state, region', 'required'),
+            array('fullNameUk, fullNameEn, shortNameUk, shortNameEn', 'unique'),
         ));
+    }
+
+    /**
+     * This returns the name of the collection for this class
+     *
+     * @return string
+     */
+    public function getCollectionName()
+    {
+        return 'school';
+    }
+
+    /**
+     * List of collection indexes
+     *
+     * @return array
+     */
+    public function indexes()
+    {
+        return array_merge(parent::indexes(), array(
+            'fullNameUk' => array(
+                'key' => array(
+                    'fullNameUk' => \EMongoCriteria::SORT_ASC,
+                ),
+                'unique' => true,
+            ),
+            'fullNameEn' => array(
+                'key' => array(
+                    'fullNameEn' => \EMongoCriteria::SORT_ASC,
+                ),
+            ),
+            'shortNameUk' => array(
+                'key' => array(
+                    'shortNameUk' => \EMongoCriteria::SORT_ASC,
+                ),
+            ),
+            'shortNameEn' => array(
+                'key' => array(
+                    'shortNameEn' => \EMongoCriteria::SORT_ASC,
+                ),
+            ),
+        ));
+    }
+
+    /**
+     * Before validate action
+     *
+     * @return bool
+     */
+    protected function beforeValidate()
+    {
+        if (!parent::beforeValidate()) return false;
+
+        // State
+        if (!in_array($this->state, Geo\State::model()->getConstantList('NAME_'))) {
+            $this->addError('state', \yii::t('app', 'Unknown state name.'));
+        }
+
+        // Region
+        if (!in_array($this->region, Geo\Region::model()->getConstantList('NAME_'))) {
+            $this->addError('region', \yii::t('app', 'Unknown region name.'));
+        }
+
+        return true;
     }
 
 }
--- console/commands/SchoolCommand.php
+++ console/commands/SchoolCommand.php
@@ -1,111 +1,22 @@
 <?php
 
-use common\models\Geo\State;
+use common\models\Geo\State,
+    \common\models\School;
 
 class SchoolCommand extends \console\ext\ConsoleCommand
 {
 
     /**
-     * list of states for urls
-     * @var array
-     */
-    private $_states = array(
-        '2'  => State::NAME_ARC,
-        '3'  => State::NAME_VINNYTSIA,
-        '4'  => State::NAME_VOLYN,
-        '5'  => State::NAME_DNIPROPETROVSK,
-        '6'  => State::NAME_DONETSK,
-        '7'  => State::NAME_ZHYTOMYR,
-        '8'  => State::NAME_ZAKARPATTIA,
-        '9'  => State::NAME_ZAPORIZHIA,
-        '10' => State::NAME_IVANO_FRANKIVSK,
-        '11' => State::NAME_KIEV,
-        '12' => State::NAME_KIROVOHRAD,
-        '13' => State::NAME_LUHANSK,
-        '14' => State::NAME_LVIV,
-        '15' => State::NAME_MYKOLAIV,
-        '16' => State::NAME_ODESSA,
-        '17' => State::NAME_POLTAVA,
-        '18' => State::NAME_RIVNE,
-        '19' => State::NAME_SUMY,
-        '20' => State::NAME_TERNOPIL,
-        '21' => State::NAME_KHARKIV,
-        '22' => State::NAME_KHERSON,
-        '23' => State::NAME_KHMELNYTSKYI,
-        '24' => State::NAME_CHERKASY,
-        '25' => State::NAME_CHERNIVTSI,
-        '26' => State::NAME_CHERNIHIV,
-        '27' => State::NAME_KIEV,
-        '28' => State::NAME_ARC,
-    );
-
-    /**
-     * list of words. Any of these should be present in the name of university
-     * @var array
-     */
-    private $_words = array('університет', 'академія', 'інститут');
-
-    /**
-     * If a university has any of these words in its name we don't save it
-     * @var array
-     */
-    private $_forbidden_words = array('коледж', 'училище', 'технікум');
-
-    /**
-     * method which parses all the pages
-     * @param null $year
-     * @return int
-     */
-    public function actionParse($year = NULL) {
-        if (!isset($year)) {
-            $year = date('Y');
-        }
-        $year = (int)$year;
-        \yii::import('common.lib.HtmlDomParser.*');
-        require_once('HtmlDomParser.php');
-
-        if (!empty($year)) {
-            foreach ($this->_states as $num=>$name) {
-                $parser = new \Sunra\PhpSimple\HtmlDomParser();
-                $parser = $parser->file_get_html('http://www.vstup.info/' . $year . '/i' . $year . 'o' . $num . '.html');
-                foreach ($parser->find('#branch2 a') as $u) {
-                    $text = $u->plaintext;
-
-//                    check for forbidden words
-                    if ($this->_hasForbiddenWords($text)) {
-                        continue;
-                    }
-
-//                    check if it has needed words
-                    if ($this->_hasNeededWord($text)) {
-
-                        $school = new \common\models\School();
-                        $school->fullNameUa = $text;
-                        $school->state = $name;
-                        $school->region = State::get($name)->region->name;
-                        $school->save();
-                    }
-                }
-                unset($parser);
-            }
-            echo \yii::t('app', 'SUCCESS! Parsed and saved.');
-            return 0;
-        } else {
-            echo \yii::t('app', 'ERROR! Year is invalid.');
-            return 1;
-        }
-
-    }
-
-    /**
-     * checks if a name contains any of forbidden words
-     * @param $text
+     * Checks if a name contains any of forbidden words
+     *
+     * @param string $text
      * @return bool
      */
-    private function _hasForbiddenWords($text) {
-        $fwords = $this->_forbidden_words;
-        foreach ($fwords as $fword) {
-            if (mb_stripos($text, $fword, null, "UTF-8") !== false) {
+    protected function _hasForbiddenWords($text)
+    {
+        $wordList = array('коледж', 'училище', 'технікум');
+        foreach ($wordList as $word) {
+            if (mb_stripos($text, $word, null, "UTF-8") !== false) {
                 return true;
             }
         }
@@ -114,17 +25,95 @@ class SchoolCommand extends \console\ext\ConsoleCommand
 
     /**
      * checks if a name contains any of needed words
-     * @param $text
+     *
+     * @param string $text
      * @return bool
      */
-    private function _hasNeededWord($text) {
-        $nwords = $this->_words;
-        foreach ($nwords as $nword) {
-            if (mb_stripos($text, $nword, null, "UTF-8") !== false) {
+    protected function _hasNeededWord($text)
+    {
+        $wordList = array('університет', 'академія', 'інститут');
+        foreach ($wordList as $word) {
+            if (mb_stripos($text, $word, null, "UTF-8") !== false) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * Parse data from vstup.info
+     *
+     * @param string $year
+     */
+    public function actionParse($year = null)
+    {
+        // Import HTML DOM Parser
+        \yii::import('common.lib.HtmlDomParser.*');
+        require_once('HtmlDomParser.php');
+
+        // Get year
+        if (empty($year)) {
+            $year = date('Y');
+        }
+        $year = (int)$year;
+
+        // List of states for URLs
+        $stateList = array(
+            '2'  => State::NAME_ARC,
+            '3'  => State::NAME_VINNYTSIA,
+            '4'  => State::NAME_VOLYN,
+            '5'  => State::NAME_DNIPROPETROVSK,
+            '6'  => State::NAME_DONETSK,
+            '7'  => State::NAME_ZHYTOMYR,
+            '8'  => State::NAME_ZAKARPATTIA,
+            '9'  => State::NAME_ZAPORIZHIA,
+            '10' => State::NAME_IVANO_FRANKIVSK,
+            '11' => State::NAME_KIEV,
+            '12' => State::NAME_KIROVOHRAD,
+            '13' => State::NAME_LUHANSK,
+            '14' => State::NAME_LVIV,
+            '15' => State::NAME_MYKOLAIV,
+            '16' => State::NAME_ODESSA,
+            '17' => State::NAME_POLTAVA,
+            '18' => State::NAME_RIVNE,
+            '19' => State::NAME_SUMY,
+            '20' => State::NAME_TERNOPIL,
+            '21' => State::NAME_KHARKIV,
+            '22' => State::NAME_KHERSON,
+            '23' => State::NAME_KHMELNYTSKYI,
+            '24' => State::NAME_CHERKASY,
+            '25' => State::NAME_CHERNIVTSI,
+            '26' => State::NAME_CHERNIHIV,
+            '27' => State::NAME_KIEV,
+            '28' => State::NAME_ARC,
+        );
+
+        // Parse each state
+        foreach ($stateList as $num => $stateName) {
+            $parser = new \Sunra\PhpSimple\HtmlDomParser();
+            $html = $parser->file_get_html('http://www.vstup.info/' . $year . '/i' . $year . 'o' . $num . '.html');
+            foreach ($html->find('#branch2 a') as $u) {
+                $text = $u->plaintext;
+                // Check for forbidden words
+                if ($this->_hasForbiddenWords($text)) {
+                    continue;
+                }
+                // Check if it has needed words
+                if (!$this->_hasNeededWord($text)) {
+                    continue;
+                }
+                $school = new School();
+                $school->setAttributes(array(
+                    'fullNameUk'    => $text,
+                    'state'         => $stateName,
+                    'region'        => State::get($stateName)->region->name,
+                ), false);
+                $school->save();
+                echo ".";
+            }
+            unset($html);
+        }
+        echo "\nSUCCESS! Parsed and saved.";
     }
 
 }
