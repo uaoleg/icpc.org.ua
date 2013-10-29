4e5a7344ccea741bf8ecfab4d404201193cb64ac
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2013-10-29 17:09:14 +0200

    #37 implemented editing profile info. but changing role and university is yet to be done

--- web/views/scripts/user/me.php
+++ web/views/scripts/user/me.php
@@ -1,58 +1,52 @@
-<div class="col-lg-offset-4 col-lg-5">
+<script type="text/javascript">
+    $(document).ready(function() {
+        new appUserMe();
+    });
+</script>
 
-    <table class="table table-bordered">
-        <tr>
-            <td>
-                <?=\yii::t('app', 'Name')?>
-            </td>
-            <td>
-                <?=$user->firstName?> <?=$user->lastName?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <?=\yii::t('app', 'Email')?>
-            </td>
-            <td>
-                <?=$user->email?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <?=\yii::t('app', 'User status')?>
-            </td>
-            <td>
-                <?php if (!empty($user->type)): ?>
-                    <?=$user->type?>
-                <?php endif; ?>
-                <?php if ($user->type === \common\models\User::ROLE_COACH): ?>
-                    <?php if ($user->isApprovedCoach): ?>
-                        <span class="label label-success">
-                            <?=\yii::t('app', 'Approved')?>
-                        </span>
-                    <?php else: ?>
-                        <span class="label label-warning">
-                            <?=\yii::t('app', 'Waiting approval')?>
-                        </span>
-                    <?php endif; ?>
-                    <br />
-                <?php endif; ?>
-                <?php if (!empty($user->coordinator)): ?>
-                    <br />
-                    <?=\yii::t('app', 'Coordination')?>: <?=$user->getAttributeLabel($user->coordinator, 'coord')?>
-                    <?php if ($user->isApprovedCoordinator): ?>
-                        <span class="label label-success">
-                            <?=\yii::t('app', 'Approved')?>
-                        </span>
-                    <?php else: ?>
-                        <span class="label label-warning">
-                            <?=\yii::t('app', 'Waiting approval')?>
-                        </span>
-                    <?php endif; ?>
-                    <br />
-                <?php endif; ?>
-            </td>
-        </tr>
-    </table>
+<div class="row">
+    <div class="col-lg-6 col-lg-offset-3">
 
+        <div class="panel panel-primary">
+
+            <div class="panel-heading">
+                Info
+            </div>
+
+            <div class="panel-body">
+                <div class="form col-lg-10 col-lg-offset-1">
+                    <div class="form-group">
+                        <label for="firstName"><?=\yii::t('app', 'First name')?></label>
+                        <input type="text" class="form-control" id="firstName" name="firstName" placeholder="<?=\yii::t('app', 'First name')?>" value="<?=$firstName?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName"><?=\yii::t('app', 'Last name')?></label>
+                        <input type="text" class="form-control" id="lastName" name="lastName" placeholder="<?=\yii::t('app', 'Last name')?>" value="<?=$lastName?>">
+                    </div>
+                    <div class="form-group">
+                        <label><?=\yii::t('app', 'Email')?></label>
+                        <p class="form-control-static"><?=$email?></p>
+                    </div>
+                    <div class="form-group">
+                        <label for="currentPassword"><?=\yii::t('app', 'Current password (if you want to change it)')?></label>
+                        <input type="password" class="form-control input-sm" id="currentPassword" name="currentPassword">
+                    </div>
+                    <div class="form-group">
+                        <label for="newPassword"><?=\yii::t('app', 'New password')?></label>
+                        <input type="password" class="form-control input-sm" id="newPassword" name="newPassword">
+                    </div>
+                    <div class="form-group">
+                        <label for="repeatNewPassword"><?=\yii::t('app', 'Repeat new password')?></label>
+                        <input type="password" class="form-control input-sm" id="repeatNewPassword" name="repeatNewPassword">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-lg btn-primary btn-save"><?=\yii::t('app', 'Save')?></button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
 </div>
