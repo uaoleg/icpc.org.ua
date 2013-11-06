3c8708c7bd92819758ce27c0d7bd58f3da02e5e5
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-11-06 15:41:55 +0200

    #68 Fixed default uploader params

--- web/www/js/plupload.helpers.js
+++ web/www/js/plupload.helpers.js
@@ -9,6 +9,7 @@ function pluploadHelpersSettings(settings) {
         runtimes:            pluploadHelpersRuntimes(),
         max_file_size:       '10mb',
         multi_selection:     false,
+        multipart_params:    {},
         flash_swf_url:       app.baseUrl + '/lib/plupload-2.0.0-beta/js/Moxie.swf',
         silverlight_xap_url: app.baseUrl + '/lib/plupload-2.0.0-beta/js/Moxie.xap'
     };
