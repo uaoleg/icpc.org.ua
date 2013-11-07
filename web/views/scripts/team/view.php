97aec0e75784ae800e4da8eacc7b0dbd74f09286
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2013-11-07 10:45:41 +0200

    #6 changed the position of Manage team button

--- web/views/scripts/team/view.php
+++ web/views/scripts/team/view.php
@@ -1,7 +1,12 @@
 <div class="row">
     <div class="col-lg-6 col-lg-offset-3">
 
-        <h1><?=$team->name?></h1>
+        <h1>
+            <?=$team->name?>
+            <?php if ($team->coachId === \yii::app()->user->id): ?>
+                <a href="<?=$this->createUrl('team/manage/id/' . $team->_id)?>" class="btn btn-primary"><?=\yii::t('app', 'Manage')?></a>
+            <?php endif; ?>
+        </h1>
         <h3><?=$team->year?></h3>
         <strong><?=\yii::t('app', 'Coach')?></strong>: <?=$coach->firstName?>&nbsp;<?=$coach->lastName?><br>
         <strong><?=\yii::t('app', 'School')?></strong>: <?=$school->fullNameUk?>
@@ -12,8 +17,6 @@
             <?php endforeach; ?>
         </ul>
 
-        <?php if ($team->coachId === \yii::app()->user->id): ?>
-        <a href="<?=$this->createUrl('team/manage/id/' . $team->_id)?>" class="btn btn-primary"><?=\yii::t('app', 'Manage')?></a>
-        <?php endif; ?>
+
     </div>
 </div>
