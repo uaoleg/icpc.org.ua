8c7703b3949cbdde1bbcae9f3cd3abf356cb503b
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-11-15 11:35:37 +0200

    #76 Fixed Year filter widget

--- web/widgets/filter/views/year.php
+++ web/widgets/filter/views/year.php
@@ -2,14 +2,14 @@
     <?php if (\yii::app()->params['yearFirst'] < date('Y')): ?>
         <button class="btn btn-default active dropdown-toggle" data-toggle="dropdown">
             <?=\yii::t('app', '{year} year', array(
-                '{year}' => $GLOBALS,
+                '{year}' => $this->checked,
             ))?>
             <span class="caret"></span>
         </button>
         <ul class="dropdown-menu" role="menu">
             <?php foreach (range(date('Y'), \yii::app()->params['yearFirst']) as $year): ?>
                 <?php if ($this->checked === $year) continue; ?>
-                <li><a href="<?=$this->createUrl('', array('year' => $year))?>"><?=\yii::t('app', '{year} year', array(
+                <li><a href="<?=\yii::app()->controller->createUrl('', array('year' => $year))?>"><?=\yii::t('app', '{year} year', array(
                     '{year}' => $year,
                 ))?></a></li>
             <?php endforeach; ?>
