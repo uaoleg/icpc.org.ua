2e6205d770fc286a70e94321f6d7bb3a473522fe
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-11-15 10:27:14 +0200

    #76 DB migration

--- console/commands/TmpCommand.php
+++ console/commands/TmpCommand.php
@@ -19,4 +19,16 @@ class TmpCommand extends \console\ext\ConsoleCommand
         echo "\nDone";
     }
 
+    /**
+     * Set geo attribute for news
+     *
+     * @version phase-2
+     */
+    public function actionNewsGeo()
+    {
+        $modifier = new \EMongoModifier();
+        $modifier->addModifier('geo', 'set', \common\models\School::model()->country);
+        \common\models\News::model()->updateAll($modifier);
+    }
+
 }
