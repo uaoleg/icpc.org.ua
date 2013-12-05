a454fea3decc50eade568d08c6cfc07cbc74c24b
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2013-12-05 16:32:31 +0200

    #135 added console command to delete user by id

---
+++ console/commands/UserCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+use \common\models\User;
+
+class UserCommand extends \console\ext\ConsoleCommand
+{
+
+    /**
+     * Method which deletes user with specified id
+     * @param string $id
+     */
+    public function actionDelete($id = null)
+    {
+        if (!isset($id)) {
+            echo "\nError! Specify id of user to delete\n";
+            exit;
+        }
+        $userToDelete = User::model()->findByPk(new \MongoId((string)$id));
+        if (isset($userToDelete)) {
+            $userToDelete->delete();
+            echo "\nUser with id=\'$id\' was successfully deleted\n";
+        } else {
+            echo "Error! User with id=$id was not found\n";
+        }
+    }
+}
