4775ce0057dce0f8c529de529f10acb98e362b52
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2013-12-26 13:49:14 +0200

    #104 Fixed html for the save button

--- web/views/scripts/user/me.php
+++ web/views/scripts/user/me.php
@@ -126,12 +126,12 @@
                             </div>
                         </div>
 
+                    </div>
                         <div class="form-group">
-                            <div class="col-lg-9 col-lg-offset-3">
+                            <div class="col-lg-9 col-lg-offset-2">
                                 <button type="submit" class="btn btn-lg btn-primary btn-save-info"><?=\yii::t('app', 'Save')?></button>
                             </div>
                         </div>
-                    </div>
 
                     <hr />
 
