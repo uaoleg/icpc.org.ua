b3d41d4b761ff9691c8363f0d13751426d60c2aa
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2013-12-26 10:19:33 +0200

    changed confirm to custom one and minor code improvement

--- web/modules/staff/views/scripts/qatags/all.php
+++ web/modules/staff/views/scripts/qatags/all.php
@@ -41,8 +41,8 @@
 
         </td>
         <td>
-            <a href="<?=$this->createUrl('/staff/qaTags/manage', array('id' => $tag->_id))?>" class="btn btn-primary"><?=\yii::t('app', 'Edit')?></a>
-            <button data-id="<?=$tag->_id?>" data-question="<?=\yii::t('app', 'There are {n} question(s) with this tag. Are you sure?', array('{n}' => $tag->questionCount))?>" class="btn btn-danger btn-delete-tag">
+            <a href="<?=$this->createUrl('manage', array('id' => $tag->_id))?>" class="btn btn-primary"><?=\yii::t('app', 'Edit')?></a>
+            <button data-id="<?=$tag->_id?>" data-confirm="<?=\yii::t('app', 'There are {n} question(s) with this tag. Are you sure?', array('{n}' => $tag->questionCount))?>" class="btn btn-danger btn-delete-tag">
                 <?=\yii::t('app', 'Delete')?>
             </button>
         </td>
--- web/www/js/app/staff/qatags/index.js
+++ web/www/js/app/staff/qatags/index.js
@@ -3,23 +3,21 @@ function appStaffQatagsIndex() {
     /**
      * Delete tag button
      */
-    $('.btn-delete-tag').on('click', function(){
+    $('.btn-delete-tag').on('confirmed', function(){
         var $btn = $(this);
-        if (confirm($btn.data('question'))) {
-            $.ajax({
-                url: app.baseUrl + '/staff/qatags/delete',
-                data: {
-                    id: $btn.data('id')
-                },
-                success: function(response) {
-                    if (response.errors) {
-                        appShowErrors(response.errors, $('.form-horizontal'));
-                    } else {
-                        $btn.closest('.tag-row').fadeOut();
-                    }
+        $.ajax({
+            url: app.baseUrl + '/staff/qatags/delete',
+            data: {
+                id: $btn.data('id')
+            },
+            success: function(response) {
+                if (response.errors) {
+                    appShowErrors(response.errors, $('.form-horizontal'));
+                } else {
+                    $btn.closest('.tag-row').fadeOut();
                 }
-            });
-        }
+            }
+        });
     });
 
 }
