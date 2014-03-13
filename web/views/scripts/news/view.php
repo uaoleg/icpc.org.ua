ed9d1deb5d23ef8836d856313788f4be1d58d00c
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2014-03-13 11:15:27 +0200

    #226 change carousel to lightbox on news/view

--- web/views/scripts/news/view.php
+++ web/views/scripts/news/view.php
@@ -1,50 +1,20 @@
-<script>
-    $(document).ready(function() {
-        new appNewsView();
-    });
-</script>
+<?php \yii::app()->getClientScript()->registerCoreScript('lightbox'); ?>
 
 <div class="page-header">
     <h1><?=\CHtml::encode($news->title)?></h1>
 </div>
 <?php if(count($imagesIds)): ?>
     <div class="row">
-        <div class="col-lg-6 col-lg-offset-3 text-center">
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <?php $i = 0; ?>
-                    <?php foreach($imagesIds as $imageId): ?>
-                        <li data-target="#carousel-example-generic" data-slide-to="<?=$i?>" class="<?=($i++===0)?'active':''?>"></li>
-                    <?php endforeach; ?>
-                </ol>
+        <div class="col-lg-12">
+            <?php $i = 0; ?>
+            <?php foreach($imagesIds as $imageId): ?>
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <?php foreach($imagesIds as $imageId): ?>
-                        <div class="item <?=($imageId === $imagesIds[0]) ? 'active' : ''?>">
-                            <img src="<?=$this->createUrl('/news/image', array('id' => $imageId))?>.jpg" width="150" alt="" class="news-view__image_item">
-                        </div>
-                    <?php endforeach; ?>
-                </div>
+                <a href="<?=$this->createUrl('/news/image', array('id' => $imageId))?>" data-lightbox="img">
+                    <img src="<?=$this->createUrl('/news/image', array('id' => $imageId))?>.jpg" width="100" alt="" class="news-view__image_item">
+                </a>
 
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 <?php endif; ?>
 <p><?=$news->content?></p>
-
-<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content text-center">
-            <img alt="">
-        </div>
-    </div>
-</div>
