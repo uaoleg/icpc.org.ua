c8f8877c630f44df5f8aa81b70678167d878aac3
unknown <kmartyshchenko@kmartyshchenko.dpk.dataart.net>
2014-09-22 16:15:53 +0300

    docs upload test fix 22.9

--- tests/thucydides/src/main/java/com/dataart/steps/UserDocsSteps.java
+++ tests/thucydides/src/main/java/com/dataart/steps/UserDocsSteps.java
@@ -162,8 +162,8 @@ public class UserDocsSteps extends ScenarioSteps {
                 waitABit(5000);
 		robot.keyPress(KeyEvent.VK_ENTER);
 		robot.keyRelease(KeyEvent.VK_ENTER);
-                //waitABit(1000);
-                getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                waitABit(5000);
+               // getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                 WebDriverWait waiting = new WebDriverWait(getDriver(), 30, 1000);
                 waiting.until(ExpectedConditions.presenceOfElementLocated(By.xpath(DocsPage.SAVE_DOCUMENT_BUTTON_XPATH)));
 
