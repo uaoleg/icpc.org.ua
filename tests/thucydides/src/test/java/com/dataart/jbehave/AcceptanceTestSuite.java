e5e088392231c533117f2fe27cd602b61837b1a2
Oleg Shchegula <oleg.shchegula@dataart.com>
2014-10-02 10:03:54 +0300

    fixed team story

--- tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
+++ tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
@@ -9,13 +9,13 @@ public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 		//findStoriesCalled("**/Login.story");
 		//findStoriesCalled("**/Import.story");
 		//findStoriesCalled("**/PasswordReset.story");
-		findStoriesCalled("**.story");
+		//findStoriesCalled("**.story");
 		//findStoriesCalled("**/UserProfile.story");
 		//findStoriesCalled("**/News.story");	
 		//findStoriesCalled("**/QA.story");
 		//findStoriesCalled("**/Test.story");
-		//findStoriesCalled("**/Team.story");
-                //findStoriesCalled("**/Docs.story");
+		findStoriesCalled("**/Team.story");
+        //findStoriesCalled("**/Docs.story");
 		
 	}
 
