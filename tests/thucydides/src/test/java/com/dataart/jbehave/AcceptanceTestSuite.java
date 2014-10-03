0930982723f865e657b8340357bed1fae4c10280
Oleg Shchegula <oleg.shchegula@dataart.com>
2014-10-03 18:17:14 +0300

    commented docs and team story

--- tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
+++ tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
@@ -5,18 +5,18 @@ import net.thucydides.jbehave.ThucydidesJUnitStories;
 public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 	
 	public AcceptanceTestSuite() {
-		findStoriesCalled("**/Registration.story");
-		findStoriesCalled("**/Login.story");
-		findStoriesCalled("**/Import.story");
-		findStoriesCalled("**/PasswordReset.story");
-		//findStoriesCalled("**.story");
-		findStoriesCalled("**/UserProfile.story");
-		findStoriesCalled("**/News.story");	
-		findStoriesCalled("**/QA.story");
+		//findStoriesCalled("**/Registration.story");
+		//findStoriesCalled("**/Login.story");
+		//findStoriesCalled("**/Import.story");
+		//findStoriesCalled("**/PasswordReset.story");
+		findStoriesCalled("**.story");
+		//findStoriesCalled("**/UserProfile.story");
+		//findStoriesCalled("**/News.story");	
+		//findStoriesCalled("**/QA.story");
 		//findStoriesCalled("**/Test.story");
-		findStoriesCalled("**/Team.story");
+		//findStoriesCalled("**/Team.story");
         //findStoriesCalled("**/Docs.story");
-		findStoriesCalled("**/ManageUser.story");
+		//findStoriesCalled("**/ManageUser.story");
 	}
 
 }
