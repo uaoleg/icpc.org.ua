066ae84b613b8e84cac3c0d1fa11ce7562ccfbd0
unknown <kirillmartoff@gmail.com>
2014-10-10 10:20:52 +0300

    10.10

--- tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
+++ tests/thucydides/src/test/java/com/dataart/jbehave/AcceptanceTestSuite.java
@@ -9,11 +9,11 @@ public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 		//findStoriesCalled("**/Login.story");
 		//findStoriesCalled("**/Import.story");
 		//findStoriesCalled("**/PasswordReset.story");
-		findStoriesCalled("**.story");
+		//findStoriesCalled("**.story");
 		//findStoriesCalled("**/UserProfile.story");
 		//findStoriesCalled("**/News.story");	
 		//findStoriesCalled("**/QA.story");
-		//findStoriesCalled("**/Test.story");
+		findStoriesCalled("**/Test.story");
 		//findStoriesCalled("**/Team.story");
 		
 	}
---
+++ tests/thucydides/src/test/resources/com/dataart/stories/consult_dictionary/Test.story
@@ -0,0 +1,4 @@
+Scenario: As user i want to be able to get to Regulation Docs page when I click on Docs dropdown menu and choose appropriate link
+Given the user is on the Login page
+When user clicks on the Docs link and chooses Regulations item
+Then user is on the Regulations Docs page
