b39c5f1b71451ac1210a211e3447d49542b93916
Oleg Poludnenko <Oleg.Poludnenko@dataart.com>
2014-11-04 10:41:01 +0200

    Code review

--- web/modules/test/controllers/TeamController.php
+++ web/modules/test/controllers/TeamController.php
@@ -5,9 +5,14 @@ use web\modules\test\ext\Controller;
 
 class TeamController extends Controller
 {
+	
+	/**
+	 * Delete all the teams with isDeleted = true from the data storage
+	 */
     public function actionDelete()
     {
         \yii::app()->cli->runCommand('team', 'removeDeleted');
         echo "Removed deleted teams from data storage";
     }
+	
 }
