da4c1c73568e83aaf817adc1bfc77c572c62b181
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2015-02-17 10:34:31 +0200

    Minor improvement

--- web/views/mail/emailConfirmation.php
+++ web/views/mail/emailConfirmation.php
@@ -1,4 +1,4 @@
-<?=\yii::t('app', 'Congrats! You\'ve been successfully registered on Ukrainian student programming olympiad website.')?>
+<?=\yii::t('app', 'Congrats! You\'ve been successfully registered on Ukrainian Collegiate Programming Contest website.')?>
 <br />
 <?=\yii::t('app', 'Please, confirm your email by following this link.')?>
 <br />
