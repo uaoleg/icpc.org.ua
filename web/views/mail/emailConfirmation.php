a9cb6f30be121e286e1548a0f9b3d12e6f651fcc
Alexey Plekhanov <alexey.plekhanov@dataart.com>
2015-02-17 10:10:59 +0200

    Minor improvement to after-signup email

--- web/views/mail/emailConfirmation.php
+++ web/views/mail/emailConfirmation.php
@@ -1,6 +1,4 @@
-<?=\yii::t('app', 'Congrats! You\'ve been successfully registered on {app}.', array(
-    '{app}' => \yii::app()->name,
-))?>
+<?=\yii::t('app', 'Congrats! You\'ve been successfully registered on Ukrainian student programming olympiad website.')?>
 <br />
 <?=\yii::t('app', 'Please, confirm your email by following this link.')?>
 <br />
